8c0104a82002f3a3b75154b07aabd75181448d8e
Parwesh Rallapalli <parweshrallapalli@gmail.com>
2025-10-31 18:11:40 -0700

    Set up content collection for projects.

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from "astro:content";
+
+const projects = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        year: z.string();
+        company: z.string().optional(),
+        role: z.string().optional(),
+        features: z.array(z.string()).optional(),
+        mediaType: z.enum(["video", "image"]),
+        mediaSrc: z.string(), // e.g. /assets/dev/bo7-trailer-clip-1.mp4
+    }),
+});
+
+export const collections = { projects };
