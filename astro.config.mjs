bc1ce75c8c8183d692700dd9260744567bb5eeeb
Parwesh Rallapalli <parweshrallapalli@gmail.com>
2025-11-02 22:22:36 -0800

    Fixed url in astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://parweshrallapalli.github.io',
+    site: 'https://kumquatninja.github.io',
     base: '/',
 });
