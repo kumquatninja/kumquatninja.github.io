fe027688f690e401b191ad87d5bcba90a6eda595
Parwesh Rallapalli <parweshrallapalli@gmail.com>
2025-11-02 22:43:09 -0800

    Installed Bulma package to avoid CDN request. Refactored site to use this local path in BaseLayout.astro instead of in the global stylesheet.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://kumquatninja.github.io',
+    site: 'https://kumquatninja.github.io/',
     base: '/',
 });
