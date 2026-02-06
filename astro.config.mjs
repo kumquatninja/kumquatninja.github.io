bd7d050f82fd1efe4ef09c97c68d68544958d02b
Parwesh Rallapalli <parweshrallapalli@gmail.com>
2026-02-06 14:13:09 -0800

    Added astro-icons integration. Added SocialIcon components to navbar.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://kumquatninja.github.io/',
     base: '/',
+    integrations: [
+        icon(), 
+    ],
 });
